955c3139283713ce2dc39806f895917ab87a45ba
Алексей Ипполитов <setgecko@gmail.com>
2022-09-06 22:57:17 +0300

    Выполнено упражнение 4.5 Практика

--- 4.5Practice/Program.cs
+++ 4.5Practice/Program.cs
@@ -10,9 +10,40 @@ namespace _4._5Practice
     {
         static void Main(string[] args)
         {
+            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+            for (int k = 0; k < 3; k++)
+            {                
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
 
+                Console.WriteLine("Введите фамилию");
+                User.LastName = Console.ReadLine();
 
-            Console.ReadKey();
+                Console.WriteLine("Введите логин");
+                User.Login = Console.ReadLine();
+                User.LoginLength = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                var result = Console.ReadLine();
+                if (result == "Да")
+                {
+                    User.HasPet = true;
+                }
+                else
+                {
+                    User.HasPet = false;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+                User.Age = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Введите 3 любимых цвета пользователя");
+                User.favcolors = new string[3];
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
+            }            
         }
     }
 }
